d1c589e31847039a432468b94778aae885a71e63
alaabelhadj <alaa.belhadj@esprit.tn>
2024-11-21 15:31:36 +0100

    category fixed

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,34 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  Post, 
+  Body, 
+  Patch, 
+  Param, 
+  Delete, 
+  HttpStatus, 
+  HttpCode 
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  /**
+   * Crée une nouvelle catégorie.
+   */
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
+  /**
+   * Récupère toutes les catégories.
+   */
   @Get()
-  findAll() {
+  async findAll() {
     return this.categoryService.findAll();
   }
 
+  /**
+   * Récupère une catégorie par son ID.
+   */
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return this.categoryService.findOne(id);
   }
 
+  /**
+   * Met à jour une catégorie par son ID.
+   */
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoryService.update(+id, updateCategoryDto);
+  async update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+    return this.categoryService.update(id, updateCategoryDto);
   }
 
+  /**
+   * Supprime une catégorie par son ID.
+   */
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id') id: string) {
+    await this.categoryService.remove(id);
   }
 }
